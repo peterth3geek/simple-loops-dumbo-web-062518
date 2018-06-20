5e58ad3f3b9274b91d7dd2f337e768d13fe636ea
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-20 17:18:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,7 @@ def loop_iterator(number_of_times)
   loop do
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
-  if counter == number_of_times
-    break
+  break if counter == number_of_times
   end
 end
 end
